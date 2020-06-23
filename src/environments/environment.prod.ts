da5d8b712d56adead62a3c2ebcc8b09621fca1a8
luislizard <luislizard@ymail.com>
2020-06-23 09:44:46 -0300

    base href changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+    firebase: {
+    apiKey: 'AIzaSyAcsS62U0O7jprbsJyllsqa1q4ZmgbdNCE',
+    authDomain: 'encurtador-lizard.firebaseapp.com',
+    databaseURL: 'https://encurtador-lizard.firebaseio.com',
+    projectId: 'encurtador-lizard',
+    storageBucket: 'encurtador-lizard.appspot.com',
+    messagingSenderId: '133952424238',
+    appId: "1:133952424238:web:4fe5f139b64708f67292bf",
+    measurementId: "G-Y1GX4MP5VV"
+  }
 };
